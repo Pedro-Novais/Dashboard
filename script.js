6552ed678b39af49d0d315c093fdddca39c3ce7e
Pedro-Novais <phnovais7@gmail.com>
2023-09-26 10:56:48 -0300

    Quarta alteração já na main

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let h1 = document.querySelector('h1')
+let p = document.querySelector('p')
 
 h1.style.color="green";
-h1.style.fontSize="5vw";
+h1.style.fontSize="10vw";
+
+p.style.textAlign="center";
+p.style.color="green";
