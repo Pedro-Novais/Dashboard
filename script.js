7dec6fa2b71e7fe2cbe3cb6b112b4ea9741873dc
Pedro-Novais <phnovais7@gmail.com>
2023-09-26 15:53:14 -0300

    Projeto Dashboard

--- script.js
+++
@@ -1,8 +0,0 @@
-let h1 = document.querySelector('h1')
-let p = document.querySelector('p')
-
-h1.style.color="green";
-h1.style.fontSize="5vw";
-
-p.style.textAlign="center";
-p.style.color="green";
