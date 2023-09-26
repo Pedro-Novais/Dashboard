c9b5078d7f9343c96bf701062d9fb7c5d0879df8
Pedro-Novais <phnovais7@gmail.com>
2023-09-26 11:12:13 -0300

    Tentativa de push

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let h1 = document.querySelector('h1')
 let p = document.querySelector('p')
 
 h1.style.color="green";
-h1.style.fontSize="10vw";
+h1.style.fontSize="5vw";
 
 p.style.textAlign="center";
 p.style.color="green";
